1428496c0d6bea2c7bbda1e3434149855d6fa610
Anton B <antonbsa.bck@gmail.com>
2023-01-30 23:46:01 -0300

    create TimeIntervals page

---
+++ src/utils/getWeekDays.ts
@@ -0,0 +1,9 @@
+export function getWeekdays() {
+  const formatter = new Intl.DateTimeFormat('pt-BR', {
+    weekday: 'long',
+  })
+
+  return Array.from(Array(7).keys())
+    .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
+    .map((weekDay) => weekDay.charAt(0).toUpperCase() + weekDay.slice(1))
+}
